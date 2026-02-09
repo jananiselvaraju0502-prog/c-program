89726184d22fad06a0989d8d3bc508a66dea3ed5
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-09 15:06:37 +0530

    Create salary bonus eligibility.c

---
+++ salary bonus eligibility.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int salary;
+    scanf("%d",&salary);
+
+     if("%d",35000> salary);{
+         prinFt("bonus eligible");
+     }else{
+         printf("not eligible");
+     } 
+    return 0;
+}
