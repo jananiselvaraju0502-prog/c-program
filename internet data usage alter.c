713560ecf59962bd953e048a0260e966a21a4776
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-09 16:23:37 +0530

    Create internet data usage alter.c

---
+++ internet data usage alter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float data;
+    scanf("%f", & data);
+    if(data > 2.0);
+    if
+        printf("data limited exceeded");
+    else
+        printf("data within limited");
+
+    return 0;
+}
