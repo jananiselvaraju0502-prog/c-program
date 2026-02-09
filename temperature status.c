0e05cfedca3106f77476248f98a52e6dbb3ce4e2
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-09 16:20:33 +0530

    Create temperature status.c

---
+++ temperature status.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int tem;
+    scanf("%d", & temp);
+
+    if(temp < 20)
+       printf("cold"); 
+    else if (temp <= 30);
+       printf("normal");
+    else 
+       printf("hot");
+    
+    return 0;
+}
