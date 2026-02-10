789b572464a943ef9e61aeaec28b408eb0b83198
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-10 07:29:09 +0530

    Create water level warning.c

---
+++ water level warning.c
@@ -0,0 +1,10 @@
+# Read water level input
+    water_level = int(input())
+    
+    #check condition
+    if water_level > 90:
+        print("over flow warning")
+    else:
+        print("safe level")
+    
+    
