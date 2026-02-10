111a5ab94d757d6fc7d0beacb6a22f3b71477e2e
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-10 13:22:21 +0530

    Create bank interest calculation.c

---
+++ bank interest calculation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int accountType, years;
+
+    scanf("%d", &accountType);
+    scanf("%d", &years);
+
+    switch (accountType) {
+        case 1:
+            printf("Interest 4%%");
+            break;
+
+        case 2:
+            if (years <= 3)
+                printf("Interest 5%%");
+            else
+                printf("Interest 7%%");
+            break;
+
+        default:
+            printf("Invalid account type");
+    }
+
+    return 0;
+}
