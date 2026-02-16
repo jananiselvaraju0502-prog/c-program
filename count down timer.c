baa80ccf9a4083252d464f7f1ebc7e2f0f587403
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-16 13:34:05 +0530

    Create count down timer.c

---
+++ count down timer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n,a=0,b=1;
+    scanf("%d",&n);
+    for(int i=0;i<=0;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d",a);
+    }
+    
+    return 0;
+}
