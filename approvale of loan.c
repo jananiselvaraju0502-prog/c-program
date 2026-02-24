a3fb62576c78f8f7ccc64f1cffc192514ed1b57f
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-02-24 21:03:08 +0530

    Create approvale of loan.c

---
+++ approvale of loan.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int loantype, credicstore;
+
+    scanf("%d",loantype);
+    scanf("%d",credicstore);
+
+     switch(loan type) {
+        case 1:
+           if(creadicstore >= 700){
+              printf("approved");
+     }
+           else if(creditstore >= 650, && creditscore >=);
+
+    
+    return 0;
