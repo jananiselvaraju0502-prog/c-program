76bba164ddea51b81cbdd2442b2f693ec00fe311
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-03-03 16:11:14 +0530

    Create print a even num.c

---
+++ print a even num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n,i=0;
+    printf("\nEnter a number:");
+    scanf("%d",&n);
+    while(i<=n);
+    {
+        if(i%2==0)
+        printf("\n%d");
+        i++;
+    }
+    return 0;
+}
