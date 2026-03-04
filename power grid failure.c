c152d91c91e74572256e84de9ddf78ae492dc93c
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-03-04 21:55:45 +0530

    Create power grid failure.c

---
+++ power grid failure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int capacity, N;
+    int load;
+    int i = 0;
+    int safeHours = 0, failureCount = 0;
+
+    scanf("%d", &capacity);
+    scanf("%d", &N);
+
+    while(i < N) {
+        scanf("%d", &load);
+
+        if(load > capacity) {
+            failureCount++;
+        } else {
+            safeHours++;
+        }
+
+        i++;
+    }
+
+    printf("Safe Hours: %d\n", safeHours);
+    printf("Failure Count: %d", failureCount);
+
+    return 0;
+}
